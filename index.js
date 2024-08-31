1503075b28a6f8dc94741bbd521892811727b957
pshenmic <pshenmic@gmail.com>
2024-09-01 03:24:30 +0700

    Debug utxos

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ if (!process.env.RPC_PASSWORD) {
 const main = async () => {
   const privateKey = PrivateKey.fromString(process.env.PRIVATE_KEY)
   const utxos = await getAddressUtxos(process.env.ADDRESS)
+  console.log(utxos)
   const amount = utxos.reduce((acc, utxo) => utxo.satoshis + acc, 0)
 
   const transaction = new Transaction();
